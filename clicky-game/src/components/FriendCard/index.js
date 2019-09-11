e6884771a7e040c59ffcbca6d8c04fc7f81b2c7b
martucazpo <martucazpo@gmail.com>
2019-09-11 13:45:26 -0700

    initial styling

--- clicky-game/src/components/FriendCard/index.js
+++ clicky-game/src/components/FriendCard/index.js
@@ -7,7 +7,7 @@ function FriendCard(props) {
       <div className="img-container">
         <img alt={props.name} src={props.image} />
       </div>
-      <div className="content">
+      {/* <div className="content"> 
         <ul>
           <li>
             <strong>Name:</strong> {props.name}
@@ -19,10 +19,8 @@ function FriendCard(props) {
             <strong>Location:</strong> {props.location}
           </li>
         </ul>
-      </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
-      </span>
+          </div>*/}
+      <span onClick={() => props.removeFriend(props.id)} className="remove"></span>
     </div>
   );
 }
